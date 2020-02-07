2a6c7271f76d2ad8660a56e3b097a6562b2b7f03
fv316 <fv316@ic.ac.uk>
2020-02-07 17:49:59 +0100

    first commit

---
+++ download_list1.sh
@@ -0,0 +1,7 @@
+# SyncNet model
+
+mkdir data
+curl -L http://www.robots.ox.ac.uk/~vgg/data/voxceleb/meta/list_test_all.txt -O data/list_test_all.txt
+curl -L http://www.robots.ox.ac.uk/~vgg/data/voxceleb/meta/list_test_hard.txt -O data/list_test_hard.txt
+curl -L http://www.robots.ox.ac.uk/~vgg/data/voxceleb/meta/veri_test.txt -O data/veri_test.txt
+curl -L http://www.robots.ox.ac.uk/~vgg/data/voxceleb/meta/veri_test_output.txt -O data/veri_test_output.txt
